91d74d4a5fd79b9062d45fd720d27b14c0abe5b2
horbmyk <67599881+horbmyk@users.noreply.github.com>
2021-11-26 17:49:07 +0200

    Aded Timer

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,14 +4,15 @@ namespace Shake
 {
     public class GameController : MonoBehaviour
     {
+        public bool _pauseActive;
+        public bool _gameOverActive;
         [SerializeField] private FieldController _fieldController;
         [SerializeField] private SnakeController _snakeController;
         [SerializeField] private BonusBodyGrowUp _bonusBodyGrowUp;
         [SerializeField] private BonusSlowTime _bonusSlowTime;
         [SerializeField] private BonusSpeedUp _bonusSpeedUp;
         [SerializeField] private UIController _uiController;
-        public bool _pauseActive;
-        public bool _gameOver;
+        private float _timerCurentGame { get; set; }
 
         private void Awake()
         {
@@ -24,6 +25,11 @@ namespace Shake
             NewGame();
         }
 
+        private void Update()
+        {
+            _timerCurentGame += Time.deltaTime;
+        }
+
         public void NewGame()
         {
             _fieldController.CreateNewGame();
@@ -34,14 +40,15 @@ namespace Shake
             _bonusSlowTime.CreateBonusSlowTime(CONSTANTSES.BONUS_SLOW_TIME_FIELD_VALUE);
             _bonusSpeedUp.CreateBonusSpeedUp(CONSTANTSES.BONUS_SPEED_UP_FIELD_VALUE);
             _pauseActive = false;
-            _gameOver = false;
+            _gameOverActive = false;
             Time.timeScale = 1;
             _uiController.Continue();
+            _timerCurentGame = 0;
         }
 
         public void Pause()
         {
-            if (!_gameOver)
+            if (!_gameOverActive)
             {
                 if (!_pauseActive)
                 {
@@ -67,9 +74,14 @@ namespace Shake
 
         public void GameOver()
         {
-            _gameOver = true;
+            _gameOverActive = true;
             Time.timeScale = 0;
             _uiController.GameOver();
+        }
+
+        public float GetTimerCurentGame()
+        {
+            return _timerCurentGame;
         }
     }
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,10 +7,14 @@ namespace Shake
     {
         [SerializeField] private ScoreController _scoreController;
         [SerializeField] private SnakeController _snakeController;
+        [SerializeField] private GameController _gameController;
         [SerializeField] private GameObject _pause;
         [SerializeField] private GameObject _gameOver;
         [SerializeField] private Text _score;
         [SerializeField] private Text _lenth;
+        [SerializeField] private Text _timerCurentGame;
+        private string _timerFormat;
+
 
 
         private void Awake()
@@ -23,6 +27,16 @@ namespace Shake
         {
             _score.text = "Score : " + _scoreController.Score;
             _lenth.text = "Lenth : " + _snakeController.GetSnakeBody();
+            _timerCurentGame.text = "Timer " + FormateTime(_gameController.GetTimerCurentGame());
+        }
+
+        private string FormateTime(float incomingTime)
+        {
+            string timerFormat = "";
+            int seconds = (int)(incomingTime % 60);
+            int minutes = (int)(incomingTime / 60) % 60;
+            int hours = (int)(incomingTime / 3600) % 24;
+            return timerFormat = string.Format("{0:0}:{1:00}:{2:00}", hours, minutes, seconds);
         }
 
         public void Pause()
